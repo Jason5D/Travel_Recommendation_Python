bfd46bc42a6db065312ffe512e3c09477d2698ac
Jason <jason5donoghue@gmail.com>
2023-09-17 14:38:11 +0100

    added function to generate message for traveler and present attractions they might be interested in

--- tourist_rec.py
+++ tourist_rec.py
@@ -38,6 +38,7 @@ def add_attraction(destination, attraction):
     attractions_for_destination.append(attraction)
     return
 
+
 add_attraction("Los Angeles, USA", ["Venice Beach", ["beach"]])
 add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
 add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
@@ -52,6 +53,7 @@ add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]]
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
 
+
 def find_attractions(destination, interests):
     destination_index = get_destination_index(destination)
     attractions_in_city = attractions[destination_index]
@@ -63,5 +65,30 @@ def find_attractions(destination, interests):
                 attractions_with_interest.append(possible_attraction[0])
     return attractions_with_interest
 
-la_arts = find_attractions("Los Angeles, USA", ["art"])
-print(la_arts)
+
+# la_arts = find_attractions("Los Angeles, USA", ["art"])
+# print(la_arts)
+
+
+def get_attractions_for_traveler(traveler):
+    traveler_destination = traveler[1]
+    traveler_interests = traveler[2]
+    traveler_attractions = find_attractions(traveler_destination, traveler_interests)
+
+    interests_string = (
+        "Hi "
+        + traveler[0]
+        + ", we think you'll like these places around "
+        + traveler_destination
+        + ": "
+    )
+    for i in traveler_attractions:
+        interests_string += i + ", "
+
+    return interests_string[:-2]
+
+
+smills_france = get_attractions_for_traveler(
+    ["Dereck Smill", "Paris, France", ["monument"]]
+)
+print(smills_france)
