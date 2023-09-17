12e8e72a1c9f1eeb13427e84354c2598d5ac3b51
Jason <jason5donoghue@gmail.com>
2023-09-17 11:20:19 +0100

    added test objects

--- tourist_rec.py
+++ tourist_rec.py
@@ -1,1 +1,4 @@
 # Tourist recommendation app
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "Sao Paulo, Brazil", "Cairo, Eygypt"]
+
+test_traveler = ["Erin Wilkes", "Shanghai, China", ["historical site", "art"]]
